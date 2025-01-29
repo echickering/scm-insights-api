1df07c6e17f7c387dbea803eb95223803f8eb7df
echickerin <echickerin@paloaltonetworks.com>
2025-01-28 21:37:37 -0500

    Initial commit - SCM Insights API Terraform module

--- example-usage.tf
+++
@@ -1,8 +0,0 @@
-module "prisma_access_api" {
-  source         = "../modules/prisma_access_api"
-  prisma_api_key = "your_api_key"
-}
-
-output "egress_ips" {
-  value = module.prisma_access_api.egress_ips
-}
